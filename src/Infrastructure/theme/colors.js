03021428cb22f7f7d37e84e9b8ad26c2d1fe6602
tmattid <60832810+tmattid@users.noreply.github.com>
2022-06-12 20:47:30 -0700

    Initial Commit with components completed to date

---
+++ src/Infrastructure/theme/colors.js
@@ -0,0 +1,53 @@
+export const colors = {
+  brand: {
+    primary: '#2182BD',
+    secondary: '#5282BD',
+    muted: '#C6DAF7',
+  },
+  ui: {
+    primary: '#262626',
+    secondary: '#757575',
+    tertiary: '#F1F1F1',
+    quaternary: '#FFFFFF',
+    disabled: '#DEDEDE',
+    error: '#D0421B',
+    success: '#138000',
+  },
+  bg: {
+    primary: '#FFFFFF',
+    secondary: '#F1F1F1',
+  },
+  text: {
+    primary: '#262626',
+    secondary: '#757575',
+    disabled: '#9C9C9C',
+    inverse: '#FFFFFF',
+    error: '#D0421B',
+    success: '#138000',
+  },
+}
+23
+
+export const fonts = {
+  body: 'Oswald_400Regular',
+  heading: 'Lato_400Regular',
+  monospace: 'Oswald_400Regular',
+}
+
+export const fontWeights = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+}
+
+export const fontSizes = {
+  caption: '12px',
+  button: '14px',
+  body: '16px',
+  title: '20px',
+  h5: '24px',
+  h4: '34px',
+  h3: '45px',
+  h2: '56px',
+  h1: '112px',
+}
